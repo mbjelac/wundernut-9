a0a28878f07bd3ab86ee899b6ab6db8cdd4a5e7e
mbjelac <marko.bjelac@softwaresauna.com>
2018-09-05 00:51:04 +0300

    Fixed test names.

--- utils.test.js
+++ utils.test.js
@@ -1,8 +1,8 @@
 import { getLastElement } from "./utils";
 
-describe('get last row', () => {
+describe('get last element', () => {
 
-  test('thorns', () => {
+  test('throws when arguments invalid', () => {
 
     expect(() => getLastElement()).toThrow();
     expect(() => getLastElement(undefined)).toThrow();
@@ -15,7 +15,7 @@ describe('get last row', () => {
     expect(() => getLastElement([])).toThrow();
   });
 
-  test('array is unchanged', ()=>{
+  test('does not modify array', () => {
 
     const array = [1, 2, 3, 4, 5];
 
@@ -26,7 +26,7 @@ describe('get last row', () => {
     expect(array).toEqual([1, 2, 3, 4, 5]);
   });
 
-  test('gold', () => {
+  test('returns last element of array', () => {
 
     expect(getLastElement([1])).toEqual(1);
     expect(getLastElement(['foo'])).toEqual('foo');
